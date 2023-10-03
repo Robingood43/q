644779878ef3e97def9606d304dcffef812af027
Kirill Sharapov <62686901+Robingood43@users.noreply.github.com>
2023-10-03 18:37:48 +0600

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from fastapi import UploadFile, File
+from pydantic import BaseModel, EmailStr
 from db import Base
 from sqlalchemy import Column, Integer, String
 
 class Document(Base):
-
     __tablename__ = "documents"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -16,3 +16,24 @@ class Ad(BaseModel):
     title: str
     date:str
     description: str
+
+class MyUploadFile(BaseModel):
+    filename: str
+    content_type: str
+    filetext: bytes|None
+
+    @classmethod
+    async def from_uploadfile(cls, upload_file: UploadFile):
+        return cls(
+            filename= upload_file.filename,
+            content_type= upload_file.content_type,
+            filetext = await upload_file.read() if upload_file.filename else None
+        )
+
+class ContactForm(BaseModel):
+    user_name: str
+    user_email: EmailStr
+    user_phone: str
+    message: str
+    file: MyUploadFile = None
+
