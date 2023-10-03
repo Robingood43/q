ae65309c12c4736516fa1e42e915b71b4bc8b7d7
Kirill Sharapov <62686901+Robingood43@users.noreply.github.com>
2023-10-03 09:54:31 +0600

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
-from sqlalchemy import select
-
+from sqlalchemy import select, delete
 from models import Document
 
 
@@ -11,12 +10,12 @@ class Crud:
             result = await session.execute(docs)
             return result.scalars()
 
-    async def add(self, async_session:async_sessionmaker[AsyncSession], docs:Document):
+    async def add(self, async_session:async_sessionmaker[AsyncSession], docs: [Document]):
         async with async_session() as session:
-            session.add(docs)
+            session.add_all(docs)
             await session.commit()
 
-    async def delete(self, async_session:async_sessionmaker[AsyncSession], docs:Document):
+    async def delete(self, async_session:async_sessionmaker[AsyncSession], filename:list):
         async with async_session() as session:
-            await session.delete(docs)
+            await session.execute(delete(Document).where(Document.filename.in_(filename)))
             await session.commit()
