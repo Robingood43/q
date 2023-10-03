43cd0b1bd272547b9d7c1854e0d1fc58e228be17
Kirill Sharapov <62686901+Robingood43@users.noreply.github.com>
2023-10-03 12:54:59 +0600

    Update crud.py

--- crud.py
+++ crud.py
@@ -4,18 +4,18 @@ from models import Document
 
 
 class Crud:
-    async def get_all(self, async_session:async_sessionmaker[AsyncSession]):
+    async def get_all(self, async_session:  async_sessionmaker[AsyncSession]):
         async with async_session() as session:
-            docs = select(Document).order_by(Document.id)
+            docs = select(Document).order_by(Document.date)
             result = await session.execute(docs)
             return result.scalars()
 
-    async def add(self, async_session:async_sessionmaker[AsyncSession], docs: [Document]):
+    async def add(self, async_session: async_sessionmaker[AsyncSession], docs: [Document]):
         async with async_session() as session:
             session.add_all(docs)
             await session.commit()
 
-    async def delete(self, async_session:async_sessionmaker[AsyncSession], filename:list):
+    async def delete(self, async_session: async_sessionmaker[AsyncSession], filename: list):
         async with async_session() as session:
             await session.execute(delete(Document).where(Document.filename.in_(filename)))
             await session.commit()
