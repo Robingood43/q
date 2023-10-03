7c9f3158020d13a860d95f34cd724b4234b4876a
Kirill Sharapov <62686901+Robingood43@users.noreply.github.com>
2023-10-03 23:28:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import glob
-import smtplib
+import aiosmtplib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from fastapi.exceptions import HTTPException
 from fastapi import FastAPI, Request, Form, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -66,38 +65,33 @@ async def read_root(request: Request):
 
 @app.post("/send_email/")
 async def create_upload_file(user_name: str = Form(), user_email: str = Form(),user_phone: str = Form(), MESSAGE: str = Form(), file: UploadFile = Form(...)):
-    try:
-        form = ContactForm(
-            user_name=user_name,
-            user_email=user_email,
-            user_phone=user_phone,
-            message=MESSAGE,
-            file=await MyUploadFile.from_uploadfile(file)
-        )
+    form = ContactForm(
+        user_name=user_name,
+        user_email=user_email,
+        user_phone=user_phone,
+        message=MESSAGE,
+        file=await MyUploadFile.from_uploadfile(file)
+    )
 
-        msg = MIMEMultipart('mixed')
-        # Добавление текстовой части сообщения
-        text_part = MIMEText(f"Name: {form.user_name} \nemail: {form.user_email} \ntel_number: {form.user_phone} \nmessage: {form.message}", 'plain')
-        msg.attach(text_part)
-        if form.file.filename:
-            # Добавление вложения
-            attachment_part = MIMEApplication(form.file.filetext, Name=form.file.filename)
-            attachment_part['Content-Disposition'] = f'attachment; filename="{form.file.filename}"'
-            msg.attach(attachment_part)
+    msg = MIMEMultipart('mixed')
+    # Добавление текстовой части сообщения
+    text_part = MIMEText(f"Name: {form.user_name} \nemail: {form.user_email} \ntel_number: {form.user_phone} \nmessage: {form.message}", 'plain')
+    msg.attach(text_part)
+    if form.file.filename:
+        # Добавление вложения
+        attachment_part = MIMEApplication(form.file.filetext, Name=form.file.filename)
+        attachment_part['Content-Disposition'] = f'attachment; filename="{form.file.filename}"'
+        msg.attach(attachment_part)
 
-        # Заголовки сообщения
-        msg['From'] = 'sharapov.kirill.site@ya.ru'
-        msg['To'] = 'sharapov.kirill.site@ya.ru'
-        msg['Subject'] = 'Письмо с сайта'
+    # Заголовки сообщения
+    msg['From'] = 'sharapov.kirill.site@ya.ru'
+    msg['To'] = 'sharapov.kirill.site@ya.ru'
+    msg['Subject'] = 'Письмо с сайта'
 
-        # Подключение к серверу SMTP и отправка сообщения
-        with smtplib.SMTP('smtp.yandex.com', 587) as smtp:
-            smtp.starttls()
-            smtp.login('sharapov.kirill.site@ya.ru', os.getenv('PASSWORD'))
-            smtp.send_message(msg)
+    # Подключение к серверу SMTP и отправка сообщения
+    async with aiosmtplib.SMTP('smtp.yandex.com', 587) as smtp:
+        await smtp.login('sharapov.kirill.site@ya.ru', os.getenv('PASSWORD'))
+        await smtp.send_message(msg)
 
-    except Exception as e:
-        return HTTPException(400, headers= print(e))
+    return JSONResponse(status_code=200, content={"message": "Форма успешно отправлена"})
 
-    finally:
-        return JSONResponse(status_code=200, content={"message": "Форма успешно отправлена"})
